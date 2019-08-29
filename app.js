2a2b2d00e8f672c77f0634e24a5b9a3ccb560846
Kang Shiang Yap <KangShiang@Kangs-MacBook-Pro.local>
2019-08-28 22:13:21 -0700

    removed jade from app.js

--- app.js
+++ app.js
@@ -25,7 +25,6 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 // Add middleware libraries
 app.use(logger('dev'));
